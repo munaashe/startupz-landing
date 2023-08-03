8990168220c90fe07a79ccf645667fb1f7e34dc9
Denis <94230780+munaashe@users.noreply.github.com>
2023-08-03 23:30:33 +0200

    deploy changes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        domains: [],
+        unoptimized: true
+    },
+    output: 'export',
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+
